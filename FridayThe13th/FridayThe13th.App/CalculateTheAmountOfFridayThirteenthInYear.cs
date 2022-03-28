f146f7df16bbb44dee8999ef9b29193258437179
Sean Pietersen <seanpietersen7@gmail.com>
2022-03-28 14:38:46 +0200

    Feat: Added updated Test

---
+++ FridayThe13th/FridayThe13th.App/CalculateTheAmountOfFridayThirteenthInYear.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FridayThe13th.App
+{
+    public class CalculateTheAmountOfFridayThirteenthInYear : ICalculateTheAmountOfFridayThirteenthInYear
+    {
+        public string FridayTheThirteenths(int Start, int End = int.MinValue)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- FridayThe13th/FridayThe13th.Test/CalculateTheAmountOfFridayThirteenthInYearTest.cs
+++ FridayThe13th/FridayThe13th.Test/CalculateTheAmountOfFridayThirteenthInYearTest.cs
@@ -1,4 +1,4 @@
-using System;
+using FridayThe13th.App;
 using Xunit;
 
 namespace FridayThe13th.Test
@@ -9,9 +9,16 @@ namespace FridayThe13th.Test
         [InlineData(1999, 2000, "8/13/1999 10/13/2000")]
         [InlineData(2010, 2011, "8/13/2010 5/13/2011")]
         [InlineData(2012, 2013, "1/13/2012 4/13/2012 7/13/2012 9/13/2013 12/13/2013")]
-        public void FridayTheThirteenthsTest(int Start, int End = int.MinValue, string expected)
+        public void FridayTheThirteenthsTest(int Start, int End , string expected)
         {
+            //Arrange
+            ICalculateTheAmountOfFridayThirteenthInYear fridayTheThirteenths = new CalculateTheAmountOfFridayThirteenthInYear();
 
+            //Act
+            var actual = fridayTheThirteenths.FridayTheThirteenths(Start, End);
+
+            //Assert
+            Assert.Equal(expected, actual);
         }
     }
 }
